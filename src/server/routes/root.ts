e8ae6e3bdea083d5124a1cefd298ddbfbfeff8a4
DonatNathan <nathan.donat-filliod@epitech.eu>
2025-04-04 07:15:40 -0700

    [FIX] - Organise repository and update readme

--- src/server/routes/root.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (_request, response) => {
-  response.render("root", { name: "Jrob's site" });
-});
-
-export default router;
