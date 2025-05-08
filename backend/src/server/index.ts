90575ffecfb18deceff0d4deb4edfbef69f9e26c
DonatNathan <nathan.donat-filliod@epitech.eu>
2025-05-08 12:38:16 -0700

    [FIX] - Packages conflicts

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -14,12 +14,6 @@ import configureSockets from "./config/sockets";
 import * as http from "http";
 import { Server } from "socket.io";
 
-
-import { timeMiddleware } from "./middleware/time";
-import rootRoutes from "./routes/root";
-
-const pool = require("./db");
-
 dotenv.config();
 
 const app = express();
@@ -27,16 +21,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 const PORT = process.env.Port || 3000;
 
-
-const myfunc = async () => {
-  const result = await pool.query('SELECT * FROM users;');
-  console.log("Result: ", result);
-};
-
-myfunc();
-
-app.use(morgan("dev"));
-
+app.use(roomMiddleware);
 
 if(process.env.NODE_ENV !== "production") {
   const reloadServer = livereload.createServer();
@@ -58,12 +43,8 @@ configureSockets(io, app);
 
 app.use(morgan("dev"));
 
-
-app.use(cors());
 app.use(express.json());
 
-app.use(timeMiddleware);
-app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
@@ -71,7 +52,6 @@ app.use("/public", express.static(path.join(process.cwd(), "public")));
 
 app.set("views", path.join(process.cwd(), "src", "server", "views"));
 app.set("view engine", "ejs");
-
 
 app.use("/", routes.root);
 app.use("/test", routes.test);
@@ -81,20 +61,10 @@ app.use("/lobby", authMiddleware, routes.lobby);
 app.use("/chat", authMiddleware, routes.chat);
 app.use("/games", authMiddleware, routes.games);
 
-
-
-
-app.use("/api", rootRoutes);
-
-app.use(express.static(path.join(process.cwd(), "..", "frontend", "build")));
-
-
-app.get('/', (req: Request, res: Response): void => {
-  res.sendFile(path.join(process.cwd(), "..", "frontend", "build", "index.html"));
+app.use((_request, response, next) => {
+  next(httpErrors(404));
 });
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-
 });
